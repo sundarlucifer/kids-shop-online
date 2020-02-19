3146f134884c81994a956486f9a0b7c06d2135e0
sundarlucifer <sundarlucifer@gmail.com>
2020-02-20 00:25:33 +0530

    updated hosting

--- site/flutter_service_worker.js
+++ site/flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "9810ca9fc138ad2f7bf94c64cf6f0e49",
-"/main.dart.js": "8d8e8455dcce4873441493b76995fbe2",
+"/main.dart.js": "f36e7f394bad9bf22405f2ce4d70b4b1",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
